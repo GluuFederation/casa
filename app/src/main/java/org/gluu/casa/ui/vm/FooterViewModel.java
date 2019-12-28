f0d1bef39442bfb56a1d52cb3f058c41ef1c37dd
Jose G <bonustrack310@gmail.com>
2019-12-28 13:53:24 -0500

    Refactor locale management

--- app/src/main/java/org/gluu/casa/ui/vm/FooterViewModel.java
+++ app/src/main/java/org/gluu/casa/ui/vm/FooterViewModel.java
@@ -1,11 +1,16 @@
 package org.gluu.casa.ui.vm;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Comparator;
+import java.util.Optional;
 import java.util.Locale;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
 import org.gluu.casa.core.ZKService;
 import org.gluu.casa.misc.Utils;
 import org.gluu.casa.misc.WebUtils;
+import org.gluu.util.StringHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.zkoss.bind.annotation.BindingParam;
@@ -21,34 +26,41 @@ public class FooterViewModel {
 
 	private Logger logger = LoggerFactory.getLogger(getClass());
 
-	private List<Locale> locales;
-	private ZKService zkService;
+	private SortedSet<Locale> locales;
 	private Locale selectedLocale;
 
 	@Init
 	public void init() {
-		zkService = Utils.managedBean(ZKService.class);
-		// TODO: check if Set can be rendered in the listbox, if yes, change this to Set
-		locales = new ArrayList<>(zkService.getSupportedLocales());
-		// auto select English as default language
-		if (WebUtils.getServletRequest().getSession().getAttribute(Attributes.PREFERRED_LOCALE) == null) {
-			selectedLocale = locales.stream()
-					.filter(locale -> locale.getLanguage().equals(Locale.ENGLISH.getLanguage())).findAny().orElse(null);
 
-		} else {
-			selectedLocale = locales.stream().filter(locale -> locale.getLanguage().equals(
-					((Locale) WebUtils.getServletRequest().getSession().getAttribute(Attributes.PREFERRED_LOCALE))
-							.getLanguage()))
-					.findFirst().orElse(null);
+        Locale localeInSession = Optional.ofNullable(WebUtils.getServletRequest().getSession(false)
+                .getAttribute(Attributes.PREFERRED_LOCALE)).map(Locale.class::cast).orElse(null);
+        //do nothing if there is no locale in session
+	    if (localeInSession != null && locales == null) {
+            //locales null check above prevents recomputing the list to be displayed upon every page load
+            Set<Locale> tmp = Utils.managedBean(ZKService.class).getSupportedLocales();
 
-		}
-		logger.info(selectedLocale.getLanguage());
+            if (tmp != null) {
+                //Use a comparator based on locales' display name
+                locales = new TreeSet<>(Comparator.comparing(locale -> locale.getDisplayName(locale).toLowerCase()));
+                locales.addAll(tmp);
+
+                if (locales.contains(localeInSession)) {
+                    selectedLocale = localeInSession;
+                } else {
+                    //Pick a good fit based on session's locale language
+                    String language = localeInSession.getLanguage();
+                    selectedLocale = locales.stream().filter(loc -> StringHelper.equalsIgnoreCase(loc.getLanguage(), language))
+                            .findFirst().orElse(WebUtils.DEFAULT_LOCALE);
+                }
+                logger.debug("Selected locale in UI will be '{}'", selectedLocale.getDisplayName(selectedLocale));
+            }
+        }
+
 	}
 
 	@Command
-	public void languageChanged(@BindingParam("localeCode") String localeCode) {
-
-		selectedLocale = org.zkoss.util.Locales.getLocale(localeCode);
+	public void localeChanged(@BindingParam("locale") Locale locale) {
+		selectedLocale = locale;
 		WebUtils.getServletRequest().getSession().setAttribute(Attributes.PREFERRED_LOCALE, selectedLocale);
 		Executions.sendRedirect(null); // reload the same page
 	}
@@ -57,16 +69,8 @@ public class FooterViewModel {
 		return selectedLocale;
 	}
 
-	public void setSelectedLocale(Locale selectedLocale) {
-		this.selectedLocale = selectedLocale;
-	}
-
-	public List<Locale> getLocales() {
+	public Set<Locale> getLocales() {
 		return locales;
-	}
-
-	public void setLocales(List<Locale> locales) {
-		this.locales = locales;
 	}
 
 }
