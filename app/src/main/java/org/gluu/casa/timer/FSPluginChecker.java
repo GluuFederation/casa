e0485a8a63895a664c4e50b4ae0449073ba46c82
Madhumita <madhu@gluu.org>
2020-01-21 17:33:33 +0530

    Fix for Error: "Too many open files" in the "plugins" folder

--- app/src/main/java/org/gluu/casa/timer/FSPluginChecker.java
+++ app/src/main/java/org/gluu/casa/timer/FSPluginChecker.java
@@ -15,6 +15,7 @@ import javax.inject.Named;
 import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.*;
@@ -29,167 +30,181 @@ import java.util.stream.Collectors;
 @ApplicationScoped
 public class FSPluginChecker extends JobListenerSupport {
 
-    private static final int SCAN_INTERVAL = 60;    //check the plugins dir every 60sec
+	private static final int SCAN_INTERVAL = 60; // check the plugins dir every 60sec
 
-    @Inject
-    private Logger logger;
+	@Inject
+	private Logger logger;
 
-    @Inject
-    private TimerService timerService;
+	@Inject
+	private TimerService timerService;
 
-    @Inject
-    private ExtensionsManager extManager;
+	@Inject
+	private ExtensionsManager extManager;
 
-    private String jobName;
+	private String jobName;
 
-    private Path pluginsRoot;
+	private Path pluginsRoot;
 
-    private Map<String, Long> timeStamps;
+	private Map<String, Long> timeStamps;
 
-    private List<Pair<String, File>> contents;
+	private List<Pair<String, File>> contents;
 
-    public void activate(int gap) {
+	public void activate(int gap) {
 
-        try {
-            if (extManager.getPluginsRoot() != null) {
-                timerService.addListener(this, jobName);
-                //Start in 2 seconds and repeat indefinitely
-                timerService.schedule(jobName, gap, -1, SCAN_INTERVAL);
-            }
-        } catch (Exception e) {
-            logger.error(e.getMessage(), e);
-        }
+		try {
+			if (extManager.getPluginsRoot() != null) {
+				timerService.addListener(this, jobName);
+				// Start in 2 seconds and repeat indefinitely
+				timerService.schedule(jobName, gap, -1, SCAN_INTERVAL);
+			}
+		} catch (Exception e) {
+			logger.error(e.getMessage(), e);
+		}
 
-    }
+	}
 
-    public boolean removePluginFile(String pluginId) {
+	public boolean removePluginFile(String pluginId) {
 
-        boolean success = false;
-        File f = contents.stream().filter(pair -> pair.getX().equals(pluginId)).findFirst().map(Pair::getY).orElse(null);
-        try {
-            success = f != null && f.delete();
-        } catch (Exception e) {
-            logger.error(e.getMessage(), e);
-        }
-        return success;
+		boolean success = false;
+		File f = contents.stream().filter(pair -> pair.getX().equals(pluginId)).findFirst().map(Pair::getY)
+				.orElse(null);
+		try {
+			success = f != null && f.delete();
+		} catch (Exception e) {
+			logger.error(e.getMessage(), e);
+		}
+		return success;
 
-    }
+	}
 
-    @Override
-    public String getName() {
-        return jobName;
-    }
+	@Override
+	public String getName() {
+		return jobName;
+	}
 
-    @Override
-    public void jobToBeExecuted(JobExecutionContext context) {
+	@Override
+	public void jobToBeExecuted(JobExecutionContext context) {
 
-        logger.info("FSPluginChecker. Running timer job");
-        //Obtain list of jar files in plugins root
-        List<Pair<String, File>> currContents = getCurrentContents();
+		logger.info("FSPluginChecker. Running timer job");
+		// Obtain list of jar files in plugins root
+		List<Pair<String, File>> currContents = getCurrentContents();
 
-        //Contrast currContents vs. contents
+		// Contrast currContents vs. contents
 
-        List<Pair<String, File>> toDelete = new ArrayList<>();
-        //Search those in contents and not in currContents
-        for (Pair<String, File> pair : contents) {
-            String pluginId = pair.getX();
-            if (currContents.stream().noneMatch(p -> p.getX().equals(pluginId))) {
-                toDelete.add(pair);
-                logger.info("File for plugin {} seems to have been deleted", pluginId);
-            }
-        }
+		List<Pair<String, File>> toDelete = new ArrayList<>();
+		// Search those in contents and not in currContents
+		for (Pair<String, File> pair : contents) {
+			String pluginId = pair.getX();
+			if (currContents.stream().noneMatch(p -> p.getX().equals(pluginId))) {
+				toDelete.add(pair);
+				logger.info("File for plugin {} seems to have been deleted", pluginId);
+			}
+		}
 
-        List<Pair<String, File>> toAdd = new ArrayList<>();
-        //Search those in currContents and not in contents
-        for (Pair<String, File> pair : currContents) {
-            String pluginId = pair.getX();
-            if (contents.stream().noneMatch(p -> p.getX().equals(pluginId))) {
-                toAdd.add(pair);
-                logger.info("File for plugin {} seems to have been added", pluginId);
-            }
-        }
+		List<Pair<String, File>> toAdd = new ArrayList<>();
+		// Search those in currContents and not in contents
+		for (Pair<String, File> pair : currContents) {
+			String pluginId = pair.getX();
+			if (contents.stream().noneMatch(p -> p.getX().equals(pluginId))) {
+				toAdd.add(pair);
+				logger.info("File for plugin {} seems to have been added", pluginId);
+			}
+		}
 
-        //Search those matching but with differences in file contents
-        for (Pair<String, File> pair : contents) {
-            String pluginId = pair.getX();
-            File fileMatch = currContents.stream().filter(p -> p.getX().equals(pluginId))
-                    .findFirst().map(Pair::getY).orElse(null);
+		// Search those matching but with differences in file contents
+		for (Pair<String, File> pair : contents) {
+			String pluginId = pair.getX();
+			File fileMatch = currContents.stream().filter(p -> p.getX().equals(pluginId)).findFirst().map(Pair::getY)
+					.orElse(null);
 
-            if (fileMatch != null) {
-                String name = fileMatch.getName();
-                if (!name.equals(pair.getY().getName())
-                        || fileMatch.lastModified() != Optional.ofNullable(timeStamps.get(name)).orElse(0L)) {
-                    logger.info("File for plugin {} has changed. Plugin will be redeployed", pluginId);
-                    toDelete.add(pair);
-                    toAdd.add(new Pair<>(pluginId, fileMatch));
-                }
-            }
-        }
-        extManager.updatePlugins(toAdd, toDelete);
-        contents = currContents;
-        //Update timestamps
-        timeStamps = contents.stream().collect(Collectors.toMap(p -> p.getY().getName(), p -> p.getY().lastModified()));
+			if (fileMatch != null) {
+				String name = fileMatch.getName();
+				if (!name.equals(pair.getY().getName())
+						|| fileMatch.lastModified() != Optional.ofNullable(timeStamps.get(name)).orElse(0L)) {
+					logger.info("File for plugin {} has changed. Plugin will be redeployed", pluginId);
+					toDelete.add(pair);
+					toAdd.add(new Pair<>(pluginId, fileMatch));
+				}
+			}
+		}
+		extManager.updatePlugins(toAdd, toDelete);
+		contents = currContents;
+		// Update timestamps
+		timeStamps = contents.stream().collect(Collectors.toMap(p -> p.getY().getName(), p -> p.getY().lastModified()));
 
-    }
+	}
 
-    private List<Pair<String, File>> getCurrentContents() {
+	private List<Pair<String, File>> getCurrentContents() {
 
-        List<Pair<String, File>> currContents = new ArrayList<>();
-        logger.info("Checking files in plugins directory {}", pluginsRoot);
-        try {
-            List<Path> list = Files.list(pluginsRoot).filter(Utils::isJarFile).collect(Collectors.toList());
-            Set<String> pluginIdsList = new HashSet<>();
+		List<Pair<String, File>> currContents = new ArrayList<>();
+		logger.info("Checking files in plugins directory {}", pluginsRoot);
+		try {
+			List<Path> list = Files.list(pluginsRoot).filter(Utils::isJarFile).collect(Collectors.toList());
+			Set<String> pluginIdsList = new HashSet<>();
 
-            for (Path p : list) {
-                File f = p.toFile();
-                String name = f.getName();
+			for (Path p : list) {
+				File f = p.toFile();
+				String name = f.getName();
 
-                boolean delete = true;
-                try (JarInputStream jis = new JarInputStream(new BufferedInputStream(new FileInputStream(f)), false)) {
-                    Manifest m = jis.getManifest();
+				boolean delete = true;
+				JarInputStream jis = null;
+				try {
+					jis = new JarInputStream(new BufferedInputStream(new FileInputStream(f)), false);
+					Manifest m = jis.getManifest();
 
-                    if (m != null) {
-                        String pluginId = m.getMainAttributes().getValue("Plugin-Id");
-                        if (pluginId != null) {
-                            logger.info("File '{}' implements plugin {}", name, pluginId);
+					if (m != null) {
+						String pluginId = m.getMainAttributes().getValue("Plugin-Id");
+						if (pluginId != null) {
+							logger.info("File '{}' implements plugin {}", name, pluginId);
 
-                            if (pluginIdsList.contains(pluginId)) {
-                                logger.info("There is another file already implementing this plugin. Current file will be removed");
-                            } else {
-                                pluginIdsList.add(pluginId);
-                                currContents.add(new Pair<>(pluginId, f));
-                                delete = false;
-                            }
-                        } else {
-                            logger.info("File '{}' does not seem to implement a plugin for Casa", name);
-                        }
-                    }
-                } catch (Exception e) {
-                    logger.error(e.getMessage());
-                }
+							if (pluginIdsList.contains(pluginId)) {
+								logger.info(
+										"There is another file already implementing this plugin. Current file will be removed");
+							} else {
+								pluginIdsList.add(pluginId);
+								currContents.add(new Pair<>(pluginId, f));
+								delete = false;
+							}
+						} else {
+							logger.info("File '{}' does not seem to implement a plugin for Casa", name);
+						}
+					}
+				} catch (Exception e) {
+					logger.error(e.getMessage());
+				} finally {
+					try {
+						if (jis != null)
+						{
+							jis.close();
+						}
+					} catch (IOException ioe) {
+						logger.info(ioe.getMessage());
+						// can't do anything about it
+					}
+				}
 
-                try {
-                    if (delete) {
-                        logger.warn("Deleting file '{}'", name);
-                        Files.delete(p);
-                    }
-                } catch (Exception e) {
-                    logger.error(e.getMessage());
-                }
-            }
-        } catch (Exception e) {
-            logger.error(e.getMessage(), e);
-        }
-        return currContents;
+				try {
+					if (delete) {
+						logger.warn("Deleting file '{}'", name);
+						Files.delete(p);
+					}
+				} catch (Exception e) {
+					logger.error(e.getMessage());
+				}
+			}
+		} catch (Exception e) {
+			logger.error(e.getMessage(), e);
+		}
+		return currContents;
 
-    }
+	}
 
-    @PostConstruct
-    private void inited() {
-        jobName = getClass().getSimpleName() + "_fschecker";
-        pluginsRoot = extManager.getPluginsRoot();
-        contents = new ArrayList<>();
-        timeStamps = new HashMap<>();
-    }
+	@PostConstruct
+	private void inited() {
+		jobName = getClass().getSimpleName() + "_fschecker";
+		pluginsRoot = extManager.getPluginsRoot();
+		contents = new ArrayList<>();
+		timeStamps = new HashMap<>();
+	}
 
 }
